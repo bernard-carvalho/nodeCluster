545f95ef29c5e10ed29aff66fd19ad06e97d4e89
bernard <bernard.carvalho@ifto.edu.br>
2022-10-04 10:52:09 -0300

    adicionado loadbalancer roundrobin basico (threads)

--- singleNode/index.js
+++ singleNode/index.js
@@ -1,4 +1,6 @@
 import http, { Server } from "node:http"
+import cluster from "node:cluster"
+import { cpus } from "os"
 
 const server = http.createServer(
     function(req, res){ // é possivel substituir req por '_'
@@ -7,10 +9,26 @@ const server = http.createServer(
     }
 );//const server = http.createServer();
 
-const PORT = 8000;
-server.listen(
-    PORT, 
-    function(){
-        console.log(`Servidor iniciado em http://localhost:${PORT}`);
+const criarFilhos = () =>{
+    const processos = cpus().length;
+    console.log("iniciado processo pai "+process.pid+" para criad processos filhos");
+    for (let i = 0; i < processos; i++) {
+        cluster.fork();
     }
-);
+}
+
+const iniciarFilho = ()=>{
+    console.log("Servidor iniciado com pid "+process.pid+".");
+    const PORT = 8000;
+    server.listen(
+        PORT, 
+        function(){
+            console.log(`Servidor iniciado em http://localhost:${PORT}`);
+        }
+    );
+}
+
+if(cluster.isPrimary)
+    criarFilhos();
+else
+    iniciarFilho();
